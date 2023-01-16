434f7acfdc7cb0646a098881766cd467921ba795
viniciusoike <viniciusoike@gmail.com>
2023-01-16 14:35:50 -0300

    try: download google fonts on load
    Downloads google fonts on load. Check if this makes loading the package too slow.

--- R/showtext.R
+++ R/showtext.R
@@ -1,13 +1,19 @@
 .onLoad <- function(libname, pkgname) {
 
-  sysfonts::font_add("Poppins",
-           regular = "Poppins-Regular.ttf",
-           bold = "Poppins-Bold.ttf",
-           italic = "Poppins-Italic.ttf")
-  sysfonts::font_add("Roboto",
-           regular = "Roboto-Regular.ttf",
-           bold = "Roboto-Bold.ttf",
-           italic = "Roboto-Italic.ttf")
+  # import_pop <- sysfonts::font_add(
+  #   "Poppins",
+  #   regular = "Poppins-Regular.ttf",
+  #   bold = "Poppins-Bold.ttf",
+  #   italic = "Poppins-Italic.ttf")
+  #
+  # sysfonts::font_add(
+  #   "Roboto",
+  #   regular = "Roboto-Regular.ttf",
+  #   bold = "Roboto-Bold.ttf",
+  #   italic = "Roboto-Italic.ttf")
+
+  sysfonts::font_add_google("Poppins", family = "Poppins")
+  sysfonts::font_add_google("Roboto", family = "Roboto")
 
   showtext::showtext_auto()
 
